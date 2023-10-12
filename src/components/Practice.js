b53994fd393530d7b0053141af0856f1f5ee2a60
Himanshu-Kumar-Singh02 <66389487+Himanshu-Kumar-Singh02@users.noreply.github.com>
2023-10-12 19:59:54 +0530

    Update Practice.js
    git changes

--- src/components/Practice.js
+++ src/components/Practice.js
@@ -5,13 +5,13 @@ const Practice = () => {
         <div>
             <section className="movie-section">
                 <div className="movie-card">
+                    <figure>
                         <img src={movie.Poster} alt="" />
+                    </figure>
                     <div className="card-content">
                         <p className="title">{movie.Title}</p>
                         <p className="card-text">{movie.Released}</p>
                         <p className="card-text">{movie.Genre}</p>
-                        <p className="card-text">{movie.imdbRating}</p>
-                        <p className="card-text">{movie.Country}</p>
                         <NavLink to="/" className="back-btn">Go Back</NavLink>
                     </div>
                 </div>
